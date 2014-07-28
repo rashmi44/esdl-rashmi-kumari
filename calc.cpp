7367cbfeddfd8b839afeeea4a5caf0c81372bf69
rashmi44 <rashmi.kumari@cumminscollege.in>
2014-07-28 11:34:56 +0530

    Update calc.cpp
    updated from other account!

--- calc.cpp
+++ calc.cpp
@@ -2,10 +2,17 @@
 void main()
 {
   cout<<"Welcome to my account";
+  cout<<"Hello";
   
 }
 
-double add(double num1,double num2)
+double mul(double num1,double num2)
 {
-  return num1+num2;
+  return num1*num2;
 }
+double div(double num1,double num2)
+{
+  return num1/num2;
+}
+
+
