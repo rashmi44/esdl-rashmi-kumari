76a86b61cf605891442743521ddcff877ba5a206
rashmi44 <rashmi.kumari@cumminscollege.in>
2014-07-28 11:28:44 +0530

    Update calc.cpp
    updated again

--- calc.cpp
+++ calc.cpp
@@ -2,10 +2,16 @@
 void main()
 {
   cout<<"Welcome to my account";
+  cout<<"This is edited from master's account!";
   
 }
 
 double add(double num1,double num2)
 {
   return num1+num2;
+}
+
+double sub(double num3,double num4)
+{
+  return num3-num4;
 }
